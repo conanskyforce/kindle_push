84d7169541aa8c0d314e74527fe6b5da93f449ed
conanskyforce <conanskyforce@163.com>
2018-06-10 23:30:42 +0800

    config

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	transport:{
+		service:'163'//your service,
+		host:'smtp.163.com' //your email smtp server,
+		port: 587//port,
+		secure: true,
+		secureConnection: true,
+		auth: {
+			user://username,
+			pass://pass authorization
+		}
+	},
+	from://from ,
+	to://to your kindle mail
+}
